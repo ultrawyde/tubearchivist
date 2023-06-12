1c61540aa1355e1d125c41fe4ea8009e4e62c8c6
ultrawyde <96846632+ultrawyde@users.noreply.github.com>
2023-06-12 18:59:33 -0400

    Rework playlist detection in URL parser
    Rework playlist detection further by classifying any ID starting with "PL" as a playlist. Length check remains as a backup.

--- tubearchivist/home/src/ta/urlparser.py
+++ tubearchivist/home/src/ta/urlparser.py
@@ -80,7 +80,7 @@ class Parser:
     def _find_valid_id(self, id_str):
         """detect valid id from length of string"""
         """Set prefix list for Watch Later, Liked videos, and Favorite videos playlists"""
-        private_lists=["WL", "LL", "FL"]
+        private_lists=["WL", "LL", "FL", "PL"]
         """Check if ID starts with appropriate prefixes above"""
         if id_str.startswith(tuple(private_lists)):
             return {"type": "playlist", "url": id_str}
